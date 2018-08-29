62208fd17d8c95891e6f7af61d9110d4b1cafaf7
Farmy4700 <41956609+Farmy4700@users.noreply.github.com>
2018-08-29 18:48:41 -0500

    Add files via upload

---
+++ CSharpOOP/ConsoleApp1/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿public class Warrior
+{
+    public int height;
+    public int weight;
+        
+}
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        Warrior theGoodGuy = new Warrior();
+        theGoodGuy.height = 190;
+        theGoodGuy.weight = 80;
+
+        Warrior theBadGuy = new Warrior();
+        theBadGuy.height = 170;
+        theBadGuy.weight = 90;
+
+        System.Console.WriteLine(theGoodGuy.height);
+        System.Console.WriteLine(theBadGuy.height);
+
+
+    }
+}
